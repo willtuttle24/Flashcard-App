e130b05915a751c43a400e743ab04ee8e7204460
Will Tuttle <willtuttle24@gmail.com>
2022-12-22 19:12:40 -0600

    first attempt to get cards to display

--- src/Layout/home/home.js
+++ src/Layout/home/home.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import {listDecks} from "../../utils/api/index"
 import "home.css";
 
-//import functions for study, view, delete, and create
+//import functions for create, view study and delete - here
 
 function Home() {
     const [decks, setDecks] = useState([]);
@@ -17,11 +17,37 @@ useEffect(() => {
     loadDecks();
 }, [])
 
+
 return (
     <div className="home">
+      <CreateDeckButton />
 
+      {decks.map((deck, index) => {
+        return (
+          <div className="deck-card card mt-2" key={index}>
+            <div className="card-body">
+              <div className="d-flex justify-content-between">
+                <h5 className="card-title font-weight-bold">{deck.name}</h5>
+                <h6 className="card-subtitle text-muted">
+                  {deck.cards.length} cards
+                </h6>
+              </div>
+              <p className="card-text">{deck.description}</p>
+              <div className="d-flex">
+                <div className="mr-auto">
+                  <ViewDeckButton deck={deck} />
+                  <StudyDeckButton deck={deck} />
+                </div>
+                <div>
+                  <DeleteDeckButton deck={deck} />
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
-)
+  );
 }
 
 export default Home;
