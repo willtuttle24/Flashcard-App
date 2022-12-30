1abdddb2f44de5ac25f30f697c38f4c87a3d6f39
Will Tuttle <willtuttle24@gmail.com>
2022-12-30 13:52:57 -0600

    trying to display decks

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -3,9 +3,9 @@ import {Route, Switch} from "react-router-dom";
 import "./Layout.css";
 
 import Header from "./Header";
-import Home from "./home/home";
+import Home from "./home";
 import Study from "./decks/Study";
-import CreateDeckScreen from "./decks/CreateDeckScreen";
+import CreateDeckScreen from "./CreateDeckScreen";
 import DeckScreen from "./decks/DeckScreen";
 import EditDeckScreen from "./decks/EditDeckScreen";
 import AddCardScreen from "./cards/AddCardScreen";
