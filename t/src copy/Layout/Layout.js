933ac262da192ad5d1a9216dc5bdba1b75f888c8
Will Tuttle <willtuttle24@gmail.com>
2023-01-18 15:14:07 -0600

    some work to get qualified tests to pass

--- t/src copy/Layout/Layout.js
+++ t/src copy/Layout/Layout.js
@@ -11,14 +11,6 @@ import EditDeckScreen from "./decks/EditDeckScreen";
 import AddCardScreen from "./cards/AddCardScreen";
 import EditCardScreen from "./cards/EditCardScreen";
 import NotFound from "./NotFound";
-/*
-
-  
-           
-   
-            
-        
-              */
             
 
 function Layout() {
